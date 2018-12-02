7520c2e729a958cc8fa09bdddfe9d1a7adb7504e
Eu <eu@exemplo.com>
2018-12-02 07:08:03 -0200

    Hi

--- MyProgram.java
+++ MyProgram.java
@@ -3,7 +3,6 @@ import java.util.function.Predicate;
 
 public class MyProgram {
 
-	//hello world!
 	public static void main(String[] args) {
 
 		List<Object> list = Arrays.asList("32323", 2.2, 3, 3.4, 5, "2323", 1);
